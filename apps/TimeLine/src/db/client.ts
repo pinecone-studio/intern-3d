a8add496a62ea6de1e98b5798ea489b03a2fe8a4
25LP5320 <25lp5320@G305-CC0243.local>
2026-04-29 14:22:25 +0800

    drizzle syntax typo fixed

--- apps/TimeLine/src/db/client.ts
+++ apps/TimeLine/src/db/client.ts
@@ -2,5 +2,5 @@ import { drizzle } from 'drizzle-orm/d1'
 import { getTimelineDb } from '@/lib/d1'
 
 export function getDrizzleDb() {
-  return drizzle(getTimelineDb())/
+  return drizzle(getTimelineDb())
 }
