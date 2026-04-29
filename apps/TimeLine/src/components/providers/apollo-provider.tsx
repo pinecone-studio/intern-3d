2475f63505e7c589986f5d4429a139c0ca70ae40
25LP5459 25LP5459 <25LP5459@G302-CC0111.local>
2026-04-29 12:42:42 +0800

    done

--- apps/TimeLine/src/components/providers/apollo-provider.tsx
+++ apps/TimeLine/src/components/providers/apollo-provider.tsx
@@ -2,17 +2,12 @@
 
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
 import { ApolloProvider } from '@apollo/client/react'
-import { useMemo } from 'react'
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({ uri: '/api/graphql' }),
+})
 
 export function AppApolloProvider({ children }: { children: React.ReactNode }) {
-  const client = useMemo(
-    () =>
-      new ApolloClient({
-        cache: new InMemoryCache(),
-        link: new HttpLink({ uri: '/api/graphql' }),
-      }),
-    []
-  )
-
   return <ApolloProvider client={client}>{children}</ApolloProvider>
 }
